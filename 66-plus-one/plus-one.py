9290d8fe9ad3e17903be3c1d981db022254775a9
PRAKHAR PANDEY <151817533+prakh0@users.noreply.github.com>
2026-01-16 01:21:43 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (6.24%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,10 +1,11 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        carry = 1
         for i in range(len(digits)-1,-1,-1):
-            current_digits = digits[i] + carry
-            digits[i] = current_digits % 10
-            carry = current_digits // 10
-        if carry > 0:
-            return [1] + digits
-        return digits       
+            if digits[i] + 1 != 10:
+                digits[i] += 1
+                return digits
+            else:
+                digits[i] = 0
+            
+            if i == 0:
+                return [1] + digits
