337d8569c09be6323637d5ab227c44e4b600f2eb
PRAKHAR PANDEY <151817533+prakh0@users.noreply.github.com>
2026-02-14 18:46:54 +0530

    Time: 43 ms (89.16%) | Memory: 19.5 MB (9.12%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.py
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.py
@@ -1,19 +1,21 @@
 class Solution:
     def countValidSelections(self, nums: List[int]) -> int:
-        def simulate(i, d):
-            arr = nums[:]
-            while 0 <= i < len(arr):
-                if arr[i] == 0:
-                    i += d
-                else:
-                    arr[i] -= 1
-                    d *= -1
-                    i += d
-            return all(x == 0 for x in arr)
-
-        res = 0
+        total = sum(nums)
+        prefix = 0
+        count = 0
+        
         for i in range(len(nums)):
             if nums[i] == 0:
-                if simulate(i, 1): res += 1
-                if simulate(i, -1): res += 1
-        return res
+                L = prefix
+                R = total - prefix - nums[i]
+                
+                diff = abs(L - R)
+                
+                if diff == 0:
+                    count += 2
+                elif diff == 1:
+                    count += 1
+            
+            prefix += nums[i]
+        
+        return count
