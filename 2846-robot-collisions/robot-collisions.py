7dee582cf69d6c3e87415f87a1b610a4db3cadb4
PRAKHAR PANDEY <151817533+prakh0@users.noreply.github.com>
2026-04-01 23:15:50 +0530

    Time: 93 ms (88.69%) | Memory: 37.9 MB (79.17%) - LeetSync

---
+++ 2846-robot-collisions/robot-collisions.py
@@ -0,0 +1,28 @@
+class Solution:
+    def survivedRobotsHealths(self, positions: List[int], healths: List[int], directions: str) -> List[int]:
+        n = len(positions)
+        indices = list(range(n))
+        indices = sorted(indices ,key = lambda i: positions[i])
+
+        stack = []
+        for current in indices:
+            if directions[current] == 'R':
+                stack.append(current)
+            else:
+                while stack and healths[current] > 0:
+                    top = stack.pop()
+                    if healths[current] > healths[top]:
+                        healths[current] -= 1
+                        healths[top] = 0
+                    elif healths[current] < healths[top]:
+                        healths[current] = 0
+                        healths[top] -= 1
+                        stack.append(top)
+                    else:
+                        healths[current] = 0
+                        healths[top] = 0
+        result = []
+        for i in range(n):
+            if healths[i] > 0:
+                result.append(healths[i])
+        return result
